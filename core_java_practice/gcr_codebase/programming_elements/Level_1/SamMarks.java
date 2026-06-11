127c8afdad3c23b87a79213e2459ef314dc787d7
rajbhagour07 <rajbhagour07@gmail.com>
2026-06-11 13:03:07 +0530

    [Vishal Bhagour] add:- level_1 problem (Sam Marks)

---
+++ core_java_practice/gcr_codebase/programming_elements/Level_1/SamMarks.java
@@ -0,0 +1,11 @@
+package core_java_practice.gcr_codebase.programming_elements.Level_1;
+
+public class SamMarks {
+    public static void main(String[] args) {
+        double maths = 94;
+        double physics = 95;
+        double chemistry = 96;
+        double average = (maths + physics + chemistry) / 3;
+        System.out.println("Sam's average mark in PCM is " + average);
+    }
+}
