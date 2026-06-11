0e41278fef9bbb6f6f36aa300ddb1fd5c697d559
rajbhagour07 <rajbhagour07@gmail.com>
2026-06-11 13:00:49 +0530

    [Vishal Bhagour] add:- level_1 problem (Harry Age)

---
+++ core_java_practice/gcr_codebase/programming_elements/Level_1/HarryAge.java
@@ -0,0 +1,10 @@
+package core_java_practice.gcr_codebase.programming_elements.Level_1;
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
